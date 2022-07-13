d7d11e273d91a8015b634e329747e1eed7606f56
hheeseung <ssigu69@gmail.com>
2022-07-13 12:43:31 +0900

    refactoring js code & renaming tags

--- main.js
+++ main.js
@@ -1,47 +1,45 @@
-const listForm = document.querySelector('.list-form');
-const input = document.querySelector('.input');
-const button = document.querySelector('.button');
-const shoppingList = document.querySelector('.shopping-list');
-const removeAll = document.querySelector('.removeAll');
+const items = document.querySelector('.items');
+const input = document.querySelector('.list__input');
+const listForm = document.querySelector('.list__form');
+const removeAll = document.querySelector('.list__remove-all-btn');
 
-const onSubmit = (e) => {
+function onAdd(e) {
   e.preventDefault();
 
-  const listContainer = document.createElement('li');
-  listContainer.setAttribute('class', 'list-container');
+  const value = input.value;
+  if (value === '') {
+    input.focus();
+    return;
+  }
+  const item = createItem(value);
+  items.appendChild(item);
+  input.value = '';
+  input.focus();
+}
+
+function createItem(value) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+
+  const check = document.createElement('input');
+  check.setAttribute('type', 'checkbox');
+  check.setAttribute('class', 'item__check');
+  check.addEventListener('click', () => span.classList.toggle('checked'));
 
   const span = document.createElement('span');
-
-  const check = document.createElement('button');
-  check.setAttribute('class', 'check');
-  check.innerHTML = '<i class="fa-solid fa-check"></i>';
-  check.addEventListener('click', () => {
-    span.style.textDecoration = 'line-through';
-    listContainer.style.color = '#f5a276';
-  });
-
-  const remove = document.createElement('remove');
-  remove.setAttribute('class', 'remove');
-  remove.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-  remove.addEventListener('click', () => {
-    listContainer.remove();
-  });
-
-  const value = input.value;
-  shoppingList.appendChild(listContainer);
-  listContainer.appendChild(check);
-  listContainer.appendChild(span);
+  span.setAttribute('class', 'item__name');
   span.innerText = value;
-  listContainer.appendChild(remove);
-  input.value = '';
 
-  if (value === '') {
-    alert('살 것을 입력해주세요');
-    shoppingList.removeChild(listContainer);
-  }
-};
+  const removeBtn = document.createElement('button');
+  removeBtn.setAttribute('class', 'item__remove-btn');
+  removeBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  removeBtn.addEventListener('click', () => items.removeChild(itemRow));
 
-listForm.addEventListener('submit', onSubmit);
-removeAll.addEventListener('click', () => {
-  shoppingList.innerHTML = '';
-});
+  itemRow.appendChild(check);
+  itemRow.appendChild(span);
+  itemRow.appendChild(removeBtn);
+  return itemRow;
+}
+
+listForm.addEventListener('submit', onAdd);
+removeAll.addEventListener('click', () => (items.innerHTML = ''));
